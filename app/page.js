03d0c64192c319a2d0d5675bcd049dc2f2757d95
ii-shimul <islamshimul27@gmail.com>
2025-02-28 19:16:52 +0600

    reinitialized the project

---
+++ app/page.js
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1 className="text-9xl">Hello NextJS</h1>
+      </div>
+    </main>
+  );
+}
--- src/app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="py-10 text-center">Hello NextJS</h1>
-    </div>
-  );
-}
